82d6bacbcc22a2130c97faa4a00c0d9430229040
Antonio-Jr <antonio.jr.ssouza@gmail.com>
2022-04-12 08:54:20 -0400

    Removing dist and node_modules

--- front-api/dist/app.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Producer } from 'kafkajs';
-export declare class AppService {
-    private kafkaProducer;
-    constructor(kafkaProducer: Producer);
-    GetRecommendation(topic: any, data: any): Promise<void>;
-}
--- node-api/dist/recommendations/recommendations.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Producer } from '@nestjs/microservices/external/kafka.interface';
-export declare class RecommendationsService {
-    private kafkaProducer;
-    constructor(kafkaProducer: Producer);
-    sendMessage(topic: string, data: object): Promise<void>;
-}
